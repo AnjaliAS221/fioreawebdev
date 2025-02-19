4e39aa26e5249748319fa09ea2309cebca63bfe8
AnjaliAS221 <anjalias342@gmail.com>
2025-02-19 12:54:02 +0530

    hosting

--- config/passport.js
+++ config/passport.js
@@ -3,16 +3,18 @@ const googleStrategy = require("passport-google-oauth20").Strategy;
 const User = require("../models/userSchema");
 require("dotenv").config();
 
-passport.use(new googleStrategy({
-  clientID: process.env.GOOGLE_CLIENT_ID,
-  clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: process.env.NODE_ENV === 'production' 
-      ? 'https://yourwebsite.com/auth/google/callback' 
-      : 'http://localhost:3000/auth/google/callback',
-  scope: ['profile', 'email']
-},
-
-async (accessToken, refreshToken, profile, done) => {
+    const callbackURL =
+    process.env.NODE_ENV === 'production'
+      ? process.env.GOOGLE_CALLBACK_URL
+      : 'http://localhost:3000/auth/google/callback';
+  
+  passport.use(new googleStrategy({
+    clientID: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    callbackURL: callbackURL,
+    scope: ['profile', 'email']
+  },
+  async (accessToken, refreshToken, profile, done) => {
     try {
       let user = await User.findOne({ googleId: profile.id });
       if (user) {
@@ -20,7 +22,7 @@ async (accessToken, refreshToken, profile, done) => {
       } else {
         user = new User({
           name: profile.displayName,
-          email: profile.emails ? profile.emails[0].value : null, 
+          email: profile.emails ? profile.emails[0].value : null,
           googleId: profile.id,
         });
         await user.save();
@@ -29,8 +31,8 @@ async (accessToken, refreshToken, profile, done) => {
     } catch (error) {
       return done(error, null);
     }
-  }
-));
+  }));
+  
 
 passport.serializeUser((user, done) => {
     done(null, user.id);
